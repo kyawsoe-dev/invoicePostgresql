e701f1884c83be6a85211905d2c04c1c0a207d3d
Kyaw Soe <68783159+ookyawsoe-dev@users.noreply.github.com>
2024-05-15 13:15:29 +0630

    initial commit

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var authController = require('../controlller/auth');
 var verifyToken = require('../middleware/auth');
+var apiVerifyToken = require('../middleware/apiAuth');
 
 router.get('/login', authController.getLoginPage);
 router.get('/logoutpage',[verifyToken], authController.LogoutPage);
@@ -9,8 +10,8 @@ router.get('/profilepage', [verifyToken], authController.getProfilePage);
 
 
 router.post('/login', authController.postLogin);
-router.get('/logout', authController.Logout);
-router.get('/profile', [verifyToken], authController.getProfile);
+router.get('/logout', [apiVerifyToken], authController.Logout);
+router.get('/profile', [apiVerifyToken], authController.getProfile);
 
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var indexController = require('../controlller/index');
+var verifyToken = require('../middleware/auth');
 
-router.get('/', indexController.getIndexPage);
+router.get('/', [verifyToken],  indexController.getIndexPage);
 
 
 module.exports = router;
